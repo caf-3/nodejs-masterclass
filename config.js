d35439a2d2ad4a3b9364672ccb0605b392b0778e
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-18 23:56:10 +0200

    HTTPS port added

--- config.js
+++ config.js
@@ -10,13 +10,15 @@ const environments = {};
 
 // Staging (default) environment
 environments.stating = {
-    port: 3000,
+    httpPort: 3000,
+    httpsPort: 3001,
     envName: 'stating'
 }
 
 // Production environment
 environments.production = {
-    port: 5000,
+    httpPort: 5000,
+    httpsPort: 5001,
     envName: 'production'
 }
 
