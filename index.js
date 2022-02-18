b6febdea0b07b9a3c637d2fe70a030bd36b28e40
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-18 23:56:28 +0200

    code refactored

--- index.js
+++ index.js
@@ -1,95 +1,38 @@
 // built-in dependencies
 const http = require('http');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
+const https = require('https')
 const config = require('./config')
+const fs = require('fs');
 
-// server responses for api calls
-const server = http.createServer(function(req, res) {
-    
-    // get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
+// server handler
+const { serverHandler } = require('./serverHandler')
 
-    // get the path
-    const _path = parsedUrl.pathname;
-
-    // get the method
-    const method = req.method.toUpperCase();
-    
-    // remove forward slashes from all sides (not removing those in the middle)
-    const path = _path.replace(/^\/+|\/+$/g, '');
-    
-    // get the query string as an object
-    const queryStringObject = parsedUrl.query;
-
-    // get the headers
-    const headers = req.headers;
-
-    let buffer = ''
-    // get payload, if any
-    const decoder = new StringDecoder('utf-8');
-
-    req.on('data', function(data){
-        buffer += decoder.write(data);
-    });
-
-    req.on('end', function(){
-
-        // Choose the handler this request should go to, if this route is not defined we shoud got the not found handler
-        const chosenHandler =  router.hasOwnProperty(path) ? router[path] : handlers.notFound;
-
-        // The data object to send to the handler to send to the handler
-        const data = {
-            path: path,
-            query: queryStringObject,
-            method: method,
-            headers, headers,
-            payload: buffer
-        }
-
-        // Route the request to the handler specified in the router
-        chosenHandler(data, function(statusCode, payload){
-            // Use status code sent by the handler or use 200 as default
-            statusCode = typeof(statusCode) == 'number' || String(statusCode).length == 3 ? statusCode : 200;
-
-            // If the payload sent by the handler is not and object, we send an empty object
-            payload = typeof(payload) == "object" ? payload : {}
-            payload = JSON.stringify(payload)
-
-            // Set the response Content-Type
-            res.setHeader("Content-Type", "application/json");
-
-            // HTTP status code
-            res.writeHead(statusCode);
-
-            // The response
-            res.end(payload);
-
-            console.log({data})
-        })
-    });
+// Initialize the http server
+const httpServer = http.createServer(function(req, res) {
+    serverHandler(req, res);
 });
 
-// Define route handlers
-const handlers = {};
+const HTTP_PORT = config.httpPort;
 
-/**
- * Sample handler
- * cb stands for callback
- */
- handlers.sample = function(data, cb){
-    // Callback a http statusCode and the payload object
-    cb(401, {message: "You reached the sample route handler"})
-}
-//  For not defined handlers
-handlers.notFound = function(data, cb){
-    cb(404, {message: `Cannot find /${data.path}`})
+// Start http server
+httpServer.listen(HTTP_PORT, function(){
+    console.log(`Server listening on port ${HTTP_PORT} on ${config.envName} mode`);
+});
+
+
+// Initialize the https server
+const httpServerOptions = {
+    key: fs.readFileSync('./https/key.pem'),
+    cert: fs.readFileSync('./https/cert.pem')
 }
 
-// Define a request router 
-const router = {sample: handlers.sample}
+const httpsServer = https.createServer(httpServerOptions,function(req, res) {
+    serverHandler(req, res);
+});
 
-// server listening
-server.listen(config.port, function(){
-    console.log(`Server listening on port ${config.port} on ${config.envName} mode`);
+const HTTPS_PORT = config.httpsPort;
+
+// Start https server
+httpsServer.listen(HTTPS_PORT, function(){
+    console.log(`Server listening on port ${HTTPS_PORT} on ${config.envName} mode`);
 });
